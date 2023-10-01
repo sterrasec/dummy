953546e72e8dd238dcd4b5c5bfa0b6d552497394
tkmru <taichi.kotake@sterrasec.com>
2023-10-01 20:21:28 +0900

    Able to generate pdf

--- dummy/cli.py
+++ dummy/cli.py
@@ -4,14 +4,15 @@
 import argparse
 import binascii
 import colorama
-import pypdf
 import io
 
 from colorama import Fore, Back, Style
 from PIL import Image, ImageDraw, ImageFont
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import B5
 
-def make_png_data(text, byte_size):
-    image = Image.new('RGB', (516, 729), (255, 255, 255)) # B5, White
+def make_png(file_path, text, byte_size):
+    image = Image.new('RGB', (729, 516), (255, 255, 255)) # B5, White
     draw = ImageDraw.Draw(image)
     draw.text((10, 10), text, fill=(0, 0, 0)) # Black
     output = io.BytesIO()
@@ -19,15 +20,15 @@ def make_png_data(text, byte_size):
     png_data = output.getvalue()
 
     if len(png_data) > byte_size:
-        with open('dummy.png', 'wb') as f:
+        with open(file_path, 'wb') as f:
             f.write(png_data)
-        return None
+        return False
 
     # IEND chunk is the last chunk of a PNG file
     iend_type_index = png_data.find(b'IEND')
     if iend_type_index == -1:
         print(Fore.RED + 'Error: IEND chunk not found.')
-        return None
+        return False
 
     # The first 4 bytes of the IEND chunk are data length
     # IEND chunk type (4 bytes) follows IEND chunk data length (4 bytes)
@@ -36,7 +37,7 @@ def make_png_data(text, byte_size):
     # 12 = 4 (chunk length) + 4 (chunk type) + 4 (chunk CRC)
     extra_chunk_length = byte_size - len(png_data) - 12
     # private chunk type 
-    extra_chunk_type = b'exTr'
+    extra_chunk_type = b'eXtr'
     extra_chunk_data = b'\x00' * extra_chunk_length
     extra_chunk_crc = binascii.crc32(extra_chunk_type + extra_chunk_data)
     extra_chunk = extra_chunk_length.to_bytes(4, byteorder='big')
@@ -44,25 +45,26 @@ def make_png_data(text, byte_size):
     extra_chunk += extra_chunk_crc.to_bytes(4, byteorder='big')
 
     added_png_data = png_data[:iend_chunk_index] + extra_chunk + png_data[iend_chunk_index:]
-    return added_png_data
+    with open(file_path, 'wb') as f:
+        f.write(added_png_data)
+    return True
+
+def make_pdf(file_path, text):
+    c = canvas.Canvas(file_path, bottomup=False, pagesize=B5)
+    c.setFont('Helvetica', 30)
+    c.drawString(15, 40, text)
+    c.showPage()
+    c.save()
 
 def parse_args():
     colorama.init(autoreset=True)
     parser = argparse.ArgumentParser(description='Create a dummy file of the specified size.')
-    parser.add_argument('-type', '--type', help='File type', choices=['png', 'pdf', 'txt'], required=True)
-    parser.add_argument('-text', '--text', help='Text to be written in the file')
-    parser.add_argument('-size', '--size', help='File size (bytes)', type=int)
+    parser.add_argument('-n', '--name', help='File name(.png, .pdf)', required=True)
+    parser.add_argument('-t', '--text', help='Text to be written in the file')
+    parser.add_argument('-s', '--size', help='Bytes of file(png only)', type=int)
     args = parser.parse_args()
-    if args.type == 'png':
-        added_png_data = make_png_data(args.text, args.size)
-        with open('dummy.png', 'wb') as f:
-            f.write(added_png_data)
-    else:
-        print(Fore.RED + 'Error: Invalid file type.')
-        parser.print_help()
+    # if args.name.endswith('.png'):
 
 if __name__ == '__main__':
     colorama.init(autoreset=True)
-    png_data = make_png_data('sample png', 400001)
-    with open('dummy.png', 'wb') as f:
-        f.write(png_data)
+    make_pdf('dummy.pdf', 'sample pdf')
